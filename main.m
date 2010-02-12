b6a29cef155ac8d257aa0a98044f66945f10d6ee
Tim Kramer <tkramer@eponymousapps.com>
2010-02-11 18:56:21 -0600

    first commit

---
+++ main.m
@@ -0,0 +1,34 @@
+//
+//  main.m
+//  iPhone/MacStreamingPlayer
+//
+//  Created by Matt Gallagher on 28/10/08.
+//  Copyright Matt Gallagher 2008. All rights reserved.
+//
+//  Modified by Eponymous Applications.
+//  Copyright Eponymous Applications, LLC 2010.  All rights reserved.
+//
+//  Permission is given to use this source code file, free of charge, in any
+//  project, commercial or otherwise, entirely at your risk, with the condition
+//  that any redistribution (in part or whole) of source code must retain
+//  this copyright and permission notice. Attribution in compiled projects is
+//  appreciated but not required.
+//
+
+#ifdef TARGET_OS_IPHONE
+#import <UIKit/UIKit.h>
+#else
+#import <Cocoa/Cocoa.h>
+#endif
+
+int main(int argc, const char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+#ifdef TARGET_OS_IPHONE
+    int retVal = UIApplicationMain(argc, (char **)argv, nil, nil);
+#else
+    int retVal = NSApplicationMain(argc, argv);
+#endif
+    [pool release];
+    return retVal;
+}
